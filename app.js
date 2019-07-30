7a81c815d0e8e7b2c75f64f2898e34e94b256a09
Christopher Luby <chrismluby@gmail.com>
2019-07-30 00:34:51 -0400

    button functionality

--- app.js
+++ app.js
@@ -2,6 +2,31 @@ let add = function(a, b) {return a + b};
 let subtract = function(a, b) {return a - b};
 let multiply = function(a, b) {return a * b};
 let divide = function(a, b) {return a / b};
+let userInput = [];
+
+
+btns = document.querySelectorAll('button');
+display = document.querySelector('h2');
+clearButton = document.querySelector('.clear .clear');
+
+
+
+
+//Clear button functionality
+clearButton.addEventListener('click', function(){
+    userInput = [];
+});
+
+
+//Adds userInput functionality to each button
+btns.forEach(function(b, i,){
+    // console.log(b);
+    // console.log(i);
+    b.addEventListener('click', function(){
+        userInput.push((this.value));
+        display.innerHTML = userInput.join("")});   //Displays the array userInput as a string with no seperators.
+});
+
 
 let operator = function(op, a, b) {
     if (op === "add") {
@@ -13,10 +38,8 @@ let operator = function(op, a, b) {
     } else if (op === "multiply") {
         m = multiply(a, b)
         return m;
-} else if (op === "divide") {
+    } else if (op === "divide") {
     d = divide(a, b)
-        return d;} 
-    else {console.log("ERROR"), alert("SOMETHING WENT WRONG");}
-};    
-console.log(add(1, 2))
-console.log(operator("asdract", 1, 1));
+        return d;
+    } else {console.log("ERROR"), alert("SOMETHING WENT WRONG");}
+}; 
